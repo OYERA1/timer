6837ec62d3226ea183ecf085fa668ef0e740024a
Luan Henrique Oyera Chiacherini <7.chiacherini@gmail.com>
2022-10-04 20:29:10 -0300

    final commit

--- script.js
+++ script.js
@@ -3,19 +3,28 @@ let countDownTimer = new Date('7 Jan, 2023 15:30:00').getTime()
 var x = setInterval(function () {
     const now = new Date().getTime()
     const distance = countDownTimer - now
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24))
-    const hours = Math.floor(
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    let hours = Math.floor(
         (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     )
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
     let seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
     if (distance < 0 && distance > 0 < 10) {
         clearInterval(x)
         document.getElementById('timer').innerHTML = `chegou o grande dia`
     }
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
     if (seconds < 10) {
         seconds = `0${seconds}`
+    }
+    if (days < 10) {
+        days = `0${days}`
+    }
+    if (hours < 10) {
+        hours = `0${hours}`
     }
 
     document.getElementById('timer').innerHTML =
